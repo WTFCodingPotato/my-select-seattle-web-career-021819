71c0128eee4c6ff0e0cbd0b3b5e926548f493d5c
Joshua M Loran <joshua.loran@gmail.com>
2019-01-15 19:53:18 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
  # your code here!
+ i = 0
+ new_array = []
+ while 1 < collection.count
+   some_var = yield(collection[i])
+   if x == true
+     new_array << collection[i]
+   end
+   i += 1
+ end
+ new_array
 end
